f369c9b58dd1f649ea8830a5e02b7ce3a4252a63
ykarimi <ykarimi.2312@gmail.com>
2021-02-23 10:38:04 -0800

    added a scoreboard

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from turtle import Screen
 import time
 from snake import Snake
 from food import Food
-
-
+from scoreboard import Scoreboard
 
 screen = Screen()
+score = Scoreboard()
+score.clear()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake game")
 screen.tracer(0)
+
 
 snake = Snake()
 food = Food()
@@ -20,15 +22,17 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
+
+
 game_is_on = True
 while game_is_on:
+    score.score()
     screen.update()
     time.sleep(0.1)
     snake.move()
 
-    if snake.head.distance(food) <16 :
-      food.refresh()
-
-
+    if snake.head.distance(food) < 16:
+        score.increase()
+        food.refresh()
 
 screen.exitonclick()
