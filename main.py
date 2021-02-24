4420a3ca51a1d0acfc4cc041c493fc1e2f5e25d2
ykarimi <ykarimi.2312@gmail.com>
2021-02-23 17:53:37 -0800

    done

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ screen.bgcolor("black")
 screen.title("Snake game")
 screen.tracer(0)
 
-
 snake = Snake()
 food = Food()
 
@@ -21,8 +20,6 @@ screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
-
 
 game_is_on = True
 while game_is_on:
@@ -34,5 +31,15 @@ while game_is_on:
     if snake.head.distance(food) < 16:
         score.increase()
         food.refresh()
+        snake.extend()
+    if snake.head.xcor() < -290 or snake.head.xcor() > 290 or snake.head.ycor() < -290 or snake.head.ycor() > 290:
+        game_is_on = False
+        score.game_over()
+
+    for segment in snake.segments[1:]:
+
+        if snake.head.distance(segment) < 10:
+            score.game_over()
+            game_is_on = False
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -15,11 +15,7 @@ class Snake:
     def create(self):
 
         for i in STARATING_POSITIONS:
-            seg = Turtle(shape="square")
-            seg.color("white")
-            seg.penup()
-            seg.goto(i)
-            self.segments.append(seg)
+            self.add_seg(i)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -27,6 +23,18 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x , new_y)
         self.segments[0].forward(20)
+
+
+    def add_seg(self,position):
+        seg = Turtle(shape="square")
+        seg.color("white")
+        seg.penup()
+        seg.goto(position)
+        self.segments.append(seg)
+
+    def extend(self):
+
+        self.add_seg(self.segments[-1].position())
 
     def up(self):
         if self.head.heading() != DOWN:
